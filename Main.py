b083baf0d05fa38b4f59bd0a0e5366dce0e192d0
Seong Yeop Choi <krkr5628@gmail.com>
2024-08-22 00:09:58 +0900

    스냅샷 대기 완료 확인 및 주문 코드

--- Main.py
+++ Main.py
@@ -24,15 +24,12 @@ from sklearn.pipeline import Pipeline
 from sklearn.compose import ColumnTransformer
 from sklearn.impute import SimpleImputer
 
-import torch
-import torch.nn as nn
-
-from urllib.parse import urlencode
-import requests
-
 import jwt
-import uuid
 import hashlib
+import os
+import requests
+import uuid
+from urllib.parse import urlencode, unquote
 
 Access_Key = ""
 Secret_Key = ""
@@ -40,20 +37,16 @@ Secret_Key = ""
 def file_load() :
     file_path = "C:\\Users\\krkr5\\OneDrive\\바탕 화면\\project\\password\\upbit_setting.txt"
     data = pd.read_csv(file_path)
+    #
     Access_Key = data.loc[data['type'] == 'Access_Key', 'value'].values[0]
     Secret_Key = data.loc[data['type'] == 'Secret_Key', 'value'].values[0]
+    #
+    os.environ['UPBIT_OPEN_API_ACCESS_KEY'] = Access_Key
+    os.environ['UPBIT_OPEN_API_SECRET_KEY'] = Secret_Key
+    os.environ['UPBIT_OPEN_API_SERVER_URL'] = 'https://api.upbit.com'
+    #
     print(f"Access Key: {Access_Key}")
     print(f"Secret Key: {Secret_Key}")
-
-#파라미터가 없는 경우
-def Authorization() :
-    payload = {
-        'access_key': Access_Key,
-        'nonce': str(uuid.uuid4()),
-    }
-
-    jwt_token = jwt.encode(payload, Secret_Key)
-    authorization_token = 'Bearer {}'.format(jwt_token)
 
 #파라미터가 있는 경우(query => 파라미터의 자료형 중 배열이 존재하는 경우)
 def Authorization_Parms(parms) :
@@ -76,6 +69,7 @@ def Authorization_Parms(parms) :
     jwt_token = jwt.encode(payload, Secret_Key)
     authorization_token = 'Bearer {}'.format(jwt_token)
 
+#모든 종목 정보
 def Market_Data() :
     # API 요청 URL
     url = "https://api.upbit.com/v1/market/all"
@@ -94,6 +88,7 @@ def Market_Data() :
     else:
         print(f"Failed to retrieve data: {response.status_code}")
 
+#특정 종목 마켓 데이터 불러오기
 def Market_Data_Specific(ticker) :
     # API 요청 URL
     url = f"https://api.upbit.com/v1/ticker?markets={ticker}"
@@ -112,6 +107,7 @@ def Market_Data_Specific(ticker) :
     else:
         print(f"Failed to retrieve data: {response.status_code}")
 
+#분봉 데이터 불러오기
 def candle(type, ticker, count) :
     # API 요청 URL
     url = f"https://api.upbit.com/v1/candles/minutes/{type}?market={ticker}&count={count}"
@@ -127,16 +123,187 @@ def candle(type, ticker, count) :
         data = response.json()  # JSON 응답을 Python 객체로 변환
         df = pd.DataFrame(data)
         print(df)
-        print(df.columns)
     else:
         print(f"Failed to retrieve data: {response.status_code}")
 
+#계좌 보유 현황
+def hold_account() :
+    access_key = os.environ['UPBIT_OPEN_API_ACCESS_KEY']
+    secret_key = os.environ['UPBIT_OPEN_API_SECRET_KEY']
+    server_url = os.environ['UPBIT_OPEN_API_SERVER_URL']
+
+    payload = {
+        'access_key': access_key,
+        'nonce': str(uuid.uuid4()),
+    }
+
+    jwt_token = jwt.encode(payload, secret_key)
+    authorization = 'Bearer {}'.format(jwt_token)
+    headers = {
+        'Authorization': authorization,
+    }
+
+    response = requests.get(server_url + '/v1/accounts', headers=headers)
+    data = response.json()
+    df = pd.DataFrame(data)
+    print(df)
+
+def trasaction_history(ticker) :
+    url = f"https://api.upbit.com/v1/trades/ticks?market={ticker}&count=10"
+    headers = {"accept": "application/json"}
+    response = requests.get(url, headers=headers)
+    data = response.json()
+    df = pd.DataFrame(data)
+    print(df)
+
+#주문 가능 정보
+def order_possible(ticker) :
+
+    access_key = os.environ['UPBIT_OPEN_API_ACCESS_KEY']
+    secret_key = os.environ['UPBIT_OPEN_API_SECRET_KEY']
+    server_url = os.environ['UPBIT_OPEN_API_SERVER_URL']
+
+    params = {
+        'market': ticker
+    }
+    query_string = unquote(urlencode(params, doseq=True)).encode("utf-8")
+
+    m = hashlib.sha512()
+    m.update(query_string)
+    query_hash = m.hexdigest()
+
+    payload = {
+        'access_key': access_key,
+        'nonce': str(uuid.uuid4()),
+        'query_hash': query_hash,
+        'query_hash_alg': 'SHA512',
+    }
+
+    jwt_token = jwt.encode(payload, secret_key)
+    authorization = 'Bearer {}'.format(jwt_token)
+    headers = {
+        'Authorization': authorization,
+    }
+
+    response = requests.get(server_url + '/v1/orders/chance', params=params, headers=headers)
+    data = response.json()
+    df = pd.DataFrame(data)
+    print(df)
+
+def open_order(ticker) :
+    access_key = os.environ['UPBIT_OPEN_API_ACCESS_KEY']
+    secret_key = os.environ['UPBIT_OPEN_API_SECRET_KEY']
+    server_url = os.environ['UPBIT_OPEN_API_SERVER_URL']
+
+    params = {
+        'market': ticker,
+        'states[]': ['wait', 'watch']
+    }
+    query_string = unquote(urlencode(params, doseq=True)).encode("utf-8")
+
+    m = hashlib.sha512()
+    m.update(query_string)
+    query_hash = m.hexdigest()
+
+    payload = {
+        'access_key': access_key,
+        'nonce': str(uuid.uuid4()),
+        'query_hash': query_hash,
+        'query_hash_alg': 'SHA512',
+    }
+
+    jwt_token = jwt.encode(payload, secret_key)
+    authorization = 'Bearer {}'.format(jwt_token)
+    headers = {
+        'Authorization': authorization,
+    }
+
+    response = requests.get(server_url + '/v1/orders/open', params=params, headers=headers)
+    data = response.json()
+    df = pd.DataFrame(data)
+    print(df)
+
+def closed_order(ticker) :
+    access_key = os.environ['UPBIT_OPEN_API_ACCESS_KEY']
+    secret_key = os.environ['UPBIT_OPEN_API_SECRET_KEY']
+    server_url = os.environ['UPBIT_OPEN_API_SERVER_URL']
+
+    params = {
+        'market': ticker,
+        'states[]': ['done', 'cancel'],
+        'start_time': '2024-08-21T21:00:00+09:00',
+    }
+    query_string = unquote(urlencode(params, doseq=True)).encode("utf-8")
+
+    m = hashlib.sha512()
+    m.update(query_string)
+    query_hash = m.hexdigest()
+
+    payload = {
+        'access_key': access_key,
+        'nonce': str(uuid.uuid4()),
+        'query_hash': query_hash,
+        'query_hash_alg': 'SHA512',
+    }
+
+    jwt_token = jwt.encode(payload, secret_key)
+    authorization = 'Bearer {}'.format(jwt_token)
+    headers = {
+        'Authorization': authorization,
+    }
+
+    response = requests.get(server_url + '/v1/orders/closed', params=params, headers=headers)
+    data = response.json()
+    df = pd.DataFrame(data)
+    print(df)
+
+def order(ticker, type, ord_type, price, volume) :
+    access_key = os.environ['UPBIT_OPEN_API_ACCESS_KEY']
+    secret_key = os.environ['UPBIT_OPEN_API_SECRET_KEY']
+    server_url = os.environ['UPBIT_OPEN_API_SERVER_URL']
+
+    params = {
+        'market': ticker,
+        'side': type,
+        'ord_type': ord_type,
+        'price': price,
+        'volume': volume
+    }
+    query_string = unquote(urlencode(params, doseq=True)).encode("utf-8")
+
+    m = hashlib.sha512()
+    m.update(query_string)
+    query_hash = m.hexdigest()
+
+    payload = {
+        'access_key': access_key,
+        'nonce': str(uuid.uuid4()),
+        'query_hash': query_hash,
+        'query_hash_alg': 'SHA512',
+    }
+
+    jwt_token = jwt.encode(payload, secret_key)
+    authorization = 'Bearer {}'.format(jwt_token)
+    headers = {
+        'Authorization': authorization,
+    }
+
+    response = requests.post(server_url + '/v1/orders', json=params, headers=headers)
+    data = response.json()
+    df = pd.DataFrame(data)
+    print(df)
+
 
 def main():
-    file_load()
-    Market_Data()
-    Market_Data_Specific("KRW-BTC")
-    candle(1,"KRW-BTC",5)
+    file_load() #API KEYS LOAD
+    #Market_Data() #TOTAL TICKER
+    #Market_Data_Specific("KRW-BTC") #SPECFIC TIKCER INFO
+    #candle(1,"KRW-BTC",5) #CANDLE
+    #hold_account() #ACOUNT CASH INFO
+    #trasaction_history("KRW-BTC") #TICKER TRANSACTION INFO
+    #order_possible("KRW-SOL")
+    #open_order("KRW-SOL")
+    #closed_order("KRW-SOL")
 
 if __name__ == "__main__":
     main()
